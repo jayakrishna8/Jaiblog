25f728b3a78522fbd6b2b341197e08a956b4a8f5
jayakrishna8 <jayakrishna.nalli@gmail.com>
2019-07-19 15:22:24 +0530

    Updated enum status to blog status feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs do
+  	member do
+  		get :toggle_status
+  	end
+  end
 end
