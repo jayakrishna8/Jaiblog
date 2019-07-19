e235d84bc930bb735d0dd0f0708d6b103a9cd61d
jayakrishna8 <jayakrishna.nalli@gmail.com>
2019-07-19 16:41:11 +0530

    Implemented custom scopeto portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'angular-items', to: 'portfolios#angular'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   resources :blogs do
