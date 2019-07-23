bd2bd1963da9424e3db5f382417a473ef56cd284
jayakrishna8 <jayakrishna.nalli@gmail.com>
2019-07-24 00:48:50 +0530

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+User.create!(
+  email: "test@test.com",
+  password: "test12345",
+  password_confirmation: "test12345",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test12@test.com",
+  password: "test12345",
+  password_confirmation: "test12345",
+  name: "Regular User",
+)
+
+puts "1 Regular user created"
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
